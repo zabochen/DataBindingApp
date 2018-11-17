3570d0051ef3a158bfa760b07a722d86b0a69243
zabochen <zabochen@gmail.com>
2018-11-17 16:35:16 +0200

    Example 2

--- app/src/main/java/ua/ck/zabochen/databinding/ui/user/UserFragment.kt
+++ app/src/main/java/ua/ck/zabochen/databinding/ui/user/UserFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import ua.ck.zabochen.databinding.R
@@ -18,10 +19,12 @@ class UserFragment : Fragment() {
         }
     }
 
+    private lateinit var fragmentUserBinding: FragmentUserBinding;
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Binding
-        val fragmentUserBinding: FragmentUserBinding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_user, container, false)
+        fragmentUserBinding =
+                DataBindingUtil.inflate(inflater, R.layout.fragment_user, container, false)
 
         // Set data
         fragmentUserBinding.user = User(1, "User_1")
@@ -30,4 +33,11 @@ class UserFragment : Fragment() {
         return fragmentUserBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // User - ID
+        val userId: TextView = fragmentUserBinding.fragmentUserTextViewId
+        userId.visibility = View.GONE
+    }
 }
